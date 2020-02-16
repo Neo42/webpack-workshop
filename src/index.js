a4c652284aa2cdf9690b860f54aed955490f14f8
Jiang Hao <582307236@qq.com>
2020-02-16 11:59:49 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import makeButton from './button';
 import { makeColorStyle } from './button-styles';
-import './footer.css';
+const getFooter = () => import('./footer');
 import './button.css';
 import imageUrl from './icon.jpg';
 import makeImage from './image';
@@ -14,3 +14,7 @@ foo.blah();
 
 document.body.appendChild(button);
 document.body.appendChild(image);
+
+button.addEventListener('click', e =>
+  getFooter().then(footerModule => document.appendChild(footerModule.footer))
+);
