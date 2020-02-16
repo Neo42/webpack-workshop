c63ca0f0544a61c04657957604660d2aaf9df4b4
Jiang Hao <582307236@qq.com>
2020-02-16 12:21:50 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- build-utils/loadPresets.js
+++ build-utils/loadPresets.js
@@ -1,7 +1,8 @@
 const webpackMerge = require('webpack-merge');
 
-const applyPresets = env => {
-  const { presets } = env;
+const applyPresets = (env = { presets: [] }) => {
+  const presets = env.presets || [];
+  /** @type {string[]} */
   const mergedPresets = [].concat(...[presets]);
   const mergedConfigs = mergedPresets.map(
     presetName => require(`./presets/webpack.${presetName}`)(env) // call the preset and pass env also
